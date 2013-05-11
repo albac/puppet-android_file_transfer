5515d59a14545cfebfe169dd360bbe9bd911165c
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-05-11 10:15:00 +0200

    init commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Android File Transfer to /Applications
+#
+# Sample Usage:
+#
+#  include android_file_transfer
+#
+class android_file_transfer {
+  package { 'AndroidFileTransfer':
+    provider => 'appdmg',
+    source   => 'http://commondatastorage.googleapis.com/incrediblebee/apps/Archiver/Archiver.zip'
+  }
 }
